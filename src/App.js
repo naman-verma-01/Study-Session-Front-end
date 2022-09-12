2b3e144afadfb63146281db8d9d166d4d36c7b2d
naman-verma-01 <namanvermaca@gmail.com>
2022-09-12 22:11:06 +0530

    FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Home from "./Pages/Home"
+import Login from "./Pages/Login"
+import PostSession from "./Pages/PostSession"
+import SignUp from "./Pages/SignUp"
+import Navbar from "./Component/Navbar"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
 function App() {
+  const authStatus = useSelector((state) => state.user.authStatus);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/postsession" element={
+            <ProtectedRoute authStatus={authStatus}  >
+              <Navbar/> 
+              <PostSession />
+            </ProtectedRoute>
+          }
+          />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/" element={<><Navbar/> <Home /></>} />
+
+        </Routes>
+
+        </BrowserRouter>
+    </>
   );
 }
 
+const ProtectedRoute = ({ authStatus, children }) => {
+  console.log("AUTH_STATUS =>", authStatus);
+
+  if (!authStatus) {
+    window.alert("You need to login to access this feature.")
+    return <Navigate to="/login" replace />;
+  } else {
+    return children;
+  }
+};
 export default App;
